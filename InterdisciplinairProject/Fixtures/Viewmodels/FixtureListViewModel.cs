fc4e09fdb1ba28ab497f9c4f0ae4ae1dc49c85f6
RensThielens <rens.thielens@student.ucll.be>
2025-10-07 10:48:48 +0200

    Fixture window

--- InterdisciplinairProject/Fixtures/Viewmodels/FixtureListViewModel.cs
+++ InterdisciplinairProject/Fixtures/Viewmodels/FixtureListViewModel.cs
@@ -1,30 +1,37 @@
-﻿using CommunityToolkit.Mvvm.Input;
-using InterdisciplinairProject.Fixtures.Models;
+﻿using InterdisciplinairProject.Fixtures.Models;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using Microsoft.Win32;
+using System.Text.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Channels;
+using InterdisciplinairProject.Fixtures.Views;
 
 namespace InterdisciplinairProject.Fixtures.ViewModels
 {
-    public class FixtureListViewModel : INotifyPropertyChanged
+    public class FixtureListViewModel
     {
         public ObservableCollection<Fixture> Fixtures { get; } = new();
+        public ICommand CreateFixtureCommand { get; }
 
-        private Fixture _selectedFixture;
-        public Fixture SelectedFixture
+        public FixtureListViewModel()
         {
-            get => _selectedFixture;
-            set { _selectedFixture = value; OnPropertyChanged(); }
+            CreateFixtureCommand = new RelayCommand(CreateFixture);
         }
 
-        public ICommand CreateFixtureCommand { get; }
-        public ICommand EditChannelsCommand { get; }
-        public ICommand ImportCommand { get; }
-        public ICommand ExportCommand { get; }
+        private void CreateFixture()
+        {
+            var window = new ChannelListView
+            {
+                Owner = System.Windows.Application.Current.MainWindow
+            };
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            window.ShowDialog();
+        }
     }
 }
---
+++ InterdisciplinairProject/Fixtures/Views/ChannelListView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InterdisciplinairProject.Fixtures.Views
+{
+    /// <summary>
+    /// Interaction logic for ChannelListView.xaml
+    /// </summary>
+    public partial class ChannelListView : Window
+    {
+        public ChannelListView()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
