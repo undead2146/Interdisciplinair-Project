fbcd36b3bf66c5f1df260b317c1d06728b79d92d
Onezgamez <zeno.raymaekers@student.ucll.be>
2025-10-08 13:24:29 +0200

    mockup model

---
+++ InterdisciplinairProject.Features/Show/Model/Scene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Show.Model
+{
+    public class Scene
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Dimmer { get; set; }
+    }
+}
