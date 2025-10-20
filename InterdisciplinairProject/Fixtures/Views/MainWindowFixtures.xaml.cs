4063f6141429d1174348be43fd545e27069ee43c
Boran <zoran.bovin@student.ucll.be>
2025-10-20 19:20:07 +0200

    implemented mainView + fixes

--- InterdisciplinairProject/Fixtures/Views/MainWindowFixtures.xaml.cs
+++ InterdisciplinairProject/Fixtures/Views/MainWindowFixtures.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using InterdisciplinairProject.Fixtures.ViewModels;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace InterdisciplinairProject.Fixtures.Views
 {
     /// <summary>
     /// Interaction logic for MainWindowFixtures.xaml
     /// </summary>
-    public partial class MainWindowFixtures : Window
+    public partial class MainWindowFixtures : UserControl
     {
         public MainWindowFixtures()
         {
